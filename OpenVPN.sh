2a70f61b66212d953ae729a9bd038c91e7f77371
nadimkabbara <112121049+nadimkabbara@users.noreply.github.com>
2024-01-08 20:04:07 +0400

    Add files via upload

---
+++ OpenVPN.sh
@@ -0,0 +1,4 @@
+#!/usr/local/bin/bash
+
+
+/usr/local/bin/sudo /usr/local/sbin/openvpn --config /usr/local/etc/openvpn/my_expressvpn_uk_-_east_london_udp.ovpn --auth-user-pass /home/nkabbara/pass.txt
---
+++ clean.sh
@@ -0,0 +1,5 @@
+if [ ~/gammylog.txt ]; then
+	mv ~/gammylog.txt ~/Trash/files
+fi
+
+	mv ~/*.core ~/Trash/files
