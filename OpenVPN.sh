a63672136f0361e1244735f44fbeade556975401
nadimkabbara <112121049+nadimkabbara@users.noreply.github.com>
2024-08-13 06:02:49 +0400

    Add files via upload

--- OpenVPN.sh
+++ OpenVPN.sh
@@ -1,4 +1,5 @@
 #!/usr/local/bin/bash
 
 
-/usr/local/bin/sudo /usr/local/sbin/openvpn --config /usr/local/etc/openvpn/my_expressvpn_uk_-_east_london_udp.ovpn --auth-user-pass /home/nkabbara/pass.txt
+# /usr/local/bin/sudo /usr/local/sbin/openvpn --config /usr/local/etc/openvpn/my_expressvpn_uk_-_east_london_udp.ovpn --auth-user-pass /home/nkabbara/pass.txt
+/usr/local/bin/sudo /usr/local/sbin/openvpn --config /usr/local/etc/openvpn/my_expressvpn_lebanon_udp.ovpn --auth-user-pass /home/nkabbara/pass.txt
